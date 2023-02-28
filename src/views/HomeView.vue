2cc945959cb56bf417c104408c1825dfacd383a2
chunglijung <andy28572463@gmail.com>
2023-02-28 13:57:05 +0800

    feat: use vueuse/head 套件

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,32 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: 'HomeView',
+  name: "HomeView",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+  head() {
+    return {
+      title: "test title",
+      meta: [
+        { name: "description", content: "This is an example" },
+        { property: "og:title", content: "Hello og:title" },
+        {
+          property: "og:image",
+          content:
+            "https://yesgoimages.s3.ap-northeast-1.amazonaws.com/Original/P221012000027.jpg",
+        },
+      ],
+    };
+  },
+};
 </script>
