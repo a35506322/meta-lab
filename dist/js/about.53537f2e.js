313d1525f2dad5dbed69e8a2ca0b090aff7a19c9
chunglijung <andy28572463@gmail.com>
2023-02-28 14:11:26 +0800

    feat: add dist

---
+++ dist/js/about.53537f2e.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkmeta_lab"]=self["webpackChunkmeta_lab"]||[]).push([[443],{7381:function(a,n,t){t.r(n),t.d(n,{default:function(){return b}});var e=t(3396);const u={class:"about"},r=(0,e._)("h1",null,"This is an about page",-1),s=[r];function c(a,n){return(0,e.wg)(),(0,e.iD)("div",u,s)}var i=t(89);const l={},o=(0,i.Z)(l,[["render",c]]);var b=o}}]);
+//# sourceMappingURL=about.53537f2e.js.map
