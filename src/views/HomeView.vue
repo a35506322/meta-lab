26e379b4d4dc86caf0c3893a15423d05f3d7c15c
chunglijung <andy28572463@gmail.com>
2023-02-28 14:43:48 +0800

    feat: update dist

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,25 +8,40 @@
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
+import { useHead } from "@vueuse/head";
 
 export default {
-  name: "HomeView",
-  components: {
-    HelloWorld,
-  },
-  head() {
-    return {
-      title: "test title",
+  setup() {
+    useHead({
+      title: `Hello world: 123`,
       meta: [
         { name: "description", content: "This is an example" },
-        { property: "og:title", content: "Hello og:title" },
+        { property: "og:title", content: "Hello world" },
         {
           property: "og:image",
           content:
             "https://yesgoimages.s3.ap-northeast-1.amazonaws.com/Original/P221012000027.jpg",
         },
       ],
-    };
+    });
   },
+  name: "HomeView",
+  components: {
+    HelloWorld,
+  },
+  // head() {
+  //   return {
+  //     title: "test title",
+  //     meta: [
+  //       { name: "description", content: "This is an example" },
+  //       { property: "og:title", content: "Hello og:title" },
+  //       {
+  //         property: "og:image",
+  //         content:
+  //           "https://yesgoimages.s3.ap-northeast-1.amazonaws.com/Original/P221012000027.jpg",
+  //       },
+  //     ],
+  //   };
+  // },
 };
 </script>
